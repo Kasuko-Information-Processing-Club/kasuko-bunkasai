e76c48892f8a6c9e2042b114ebf5f12befc1338b
Manmoth <129261117+manmoth-24@users.noreply.github.com>
2025-12-17 15:24:40 +0900

    new commit

--- carousel.js
+++ carousel.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     //*****定義欄*****
 
+    //Carousel設定
     const carousel = document.querySelector("#carousel");
-
     // 1. カード要素を1つ取得
     const card = carousel.querySelector('.card');
-    
     // 2. カードの実際の幅(px)を取得
     const cardWidth = card.offsetWidth;
-    
     // 3. CSSで設定した gap: 16px の値
     const gap = 16;
-
     //要素の数
     const indexMax = 3;
+    
+
+    //Script設定
+    const intervalTime = 7000;
 
     //indexの場所にスライドする
     function ScrollToIndex(index){
